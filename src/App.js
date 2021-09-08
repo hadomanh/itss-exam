0379ca6648743384f041fa77df693f0208d1c6d1
hadomanh <hadomanh99@gmail.com>
2021-09-08 08:43:43 +0700

    Solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import list from './data.json'
 
 function App() {
+
+  const [index, setIndex] = useState(0)
+  const [students, setStudents] = useState(list)
+
+  function showStudentList() {
+    let result = '[ '
+    list.forEach((item, index) => {
+      result += item.name
+      if (index + 1 < list.length) {
+        result += ', '
+      }
+    })
+    result += ' ]'
+    return result
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <b>Student list: </b> {showStudentList()}
+      </div>
+
+      <div>
+        <b>Position: </b> {index + 1}
+      </div>
+
+      <div>
+        <b>Name: </b> {students[index].name}
+      </div>
+
+      <div>
+        {
+          index + 1 < students.length && <button onClick={() => setIndex(index + 1)}>Next</button>
+        }
+        {
+          index > 0 && <button onClick={() => setIndex(index - 1)}>Previous</button>
+        }
+      </div>
+
     </div>
   );
 }
